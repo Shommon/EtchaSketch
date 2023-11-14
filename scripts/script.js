4315d3efa206809e609dc4b3158a00f0b833d327
Shommon <sdtai2@outlook.com>
2023-11-14 12:16:18 -0500

    Fixed drag event bug when drawing, Disabled drag event in sketchcell eventListener Depreciated colorMe function, functionality added directly to event listener

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,48 @@
 const sketchContainer = document.querySelector('#sketch-container');
 const resetBtn = document.querySelector('#resetbtn')
     
-    //16x16 grid
-    let rowLength = 16;
-    let numRows = 16;
-    for (e=0; e<numRows; e++){
-        const row = document.createElement('div');
-        row.classList.add('row');
-        for (i=0; i<rowLength; i++){
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            row.appendChild(cell);
-        }
-        sketchContainer.appendChild(row)
-    };
-
-
-    const sketchCell = document.querySelectorAll('.cell')
-    let isMouseDown = false;
-    sketchCell.forEach(cell => {
-        cell.addEventListener('mouseover',sayMe);
-        console.log('attached');
-    })
-    
-    
-    function sayMe(e){
-        console.log(e.target);
-        e.target.classList.add('colored');
-    
+//16x16 grid
+let rowLength = 20;
+let numRows = 20;
+for (e=0; e<numRows; e++){
+    const row = document.createElement('div');
+    row.classList.add('row');
+    for (i=0; i<rowLength; i++){
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        row.appendChild(cell);
     }
+    sketchContainer.appendChild(row)
+};
+
+
+const sketchCell = document.querySelectorAll('.cell')
+let isMouseDown = false;
+sketchCell.forEach(cell => {
+    cell.addEventListener('mouseover',() => {
+        if (isMouseDown) {
+            cell.classList.add('colored')
+        }
+    });
+    cell.addEventListener('mousedown',() => {
+        isMouseDown = true
+        cell.classList.add('colored')
+    });
+    cell.addEventListener('mouseup', () => isMouseDown = false);
+
+    cell.addEventListener('dragstart', (e) => { //Removes Drag Events
+        e.preventDefault();
+    })
+    // console.log('attached');
+})
+
+resetBtn.addEventListener('click', resetGrid)
+
+function resetGrid(){
+    sketchCell.forEach(cell => {cell.classList.remove('colored')})
+}
+    
+    
+
 
 
